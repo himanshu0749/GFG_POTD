b759f23174f33d41b025a73342494aeffc35a033
Himanshu Bhadoria <136106889+himanshu0749@users.noreply.github.com>
2024-07-15 23:26:12 +0530

    Create Police_and_thieve.java

---
+++ Police_and_thieve.java
@@ -0,0 +1,28 @@
+ static int catchThieves(char arr[], int n, int k) 
+	{ 
+        // Code here
+        List<Integer> policemen = new ArrayList<>();
+        List<Integer> thieves = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 'P') {
+                policemen.add(i);
+            } else if (arr[i] == 'T') {
+                thieves.add(i);
+            }
+        }
+        int i = 0, j = 0;
+        int caught = 0;
+        while (i < policemen.size() && j < thieves.size()) {
+            if (Math.abs(policemen.get(i) - thieves.get(j)) <= k) {
+                caught++;
+                i++;
+                j++;
+            } else if (policemen.get(i) < thieves.get(j)) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return caught;
+	} 
